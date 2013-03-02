4349e0dd0aa86fa04dcfeeb00754a421f65652fd
zhaoxu <zhaoxu.email@gmail.com>
2013-03-02 17:24:12 +0800

    add test depencencies

---
+++ src/test/resources/cn/mobiledaily/service/UserServiceImplTest.java
@@ -0,0 +1,24 @@
+package cn.mobiledaily.service;
+
+import cn.mobiledaily.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
+
+@ContextConfiguration(locations = "classpath*:/applicationContext-test.xml")
+public class UserServiceImplTest extends AbstractTestNGSpringContextTests {
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void testRegister() throws Exception {
+        User user = userService.register("username", "password", "test@email");
+        assertThat("register user", user, notNullValue());
+        assertThat("password should be encrypted", user.getPassword(), not("password"));
+    }
+}
